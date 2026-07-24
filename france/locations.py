f703b35793f9bd9f159f886298044084d1959583
Gary Kacmarcik <garykac@gmail.com>
2026-07-24 11:28:22 -0700

    Extract LOCATIONS into separate file [France]

---
+++ france/locations.py
@@ -0,0 +1,46 @@
+# -*- coding: utf-8 -*-
+
+LOCATIONS = {
+	'Sisteron': {
+		'dept': 'd004',
+		'lat': 44.199747,
+		'long': 5.943646,
+		'desc': 'Sisteron',
+	},
+	'Eze': {
+		'dept': 'd006',
+		'lat': 43.727806,
+		'long': 7.361583,
+		'desc': 'Jardin Exotique, Eze',
+	},
+	'RoquebruneCapMartin': {
+		'dept': 'd006',
+		'lat': 43.765238,
+		'long': 7.459564,
+		'desc': 'Chateau Medieval, Roquebrune Cap Martin',
+	},
+	'Carcassone': {
+		'dept': 'd011',
+		'lat': 43.206602,
+		'long': 2.363916,
+		'desc': 'Carcassonne',
+	},
+	'NotreDameDeLaGarde': {
+		'dept': 'd013',
+		'lat': 43.284287,
+		'long': 5.371359,
+		'desc': 'Basilique Notre-Dame de la Garde, Marseille',
+	},
+	'MtStMichel': {
+		'dept': 'd050',
+		'lat': 48.63598495012916,
+		'long': -1.5113454743018682,
+		'desc': 'Mont St Michel',
+	},
+	'NotreDameDeParis': {
+		'dept': 'd075',
+		'lat': 48.853132488449546, 
+		'long': 2.349736863013811,
+		'desc': 'Cathédrale Notre-Dame de Paris',
+	},
+}
